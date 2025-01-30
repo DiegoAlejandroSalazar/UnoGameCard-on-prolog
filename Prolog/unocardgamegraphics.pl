473d67cd51a6c0a0f651e8e951d55369a54ed5a1
DiegoAlejandroSalazar <diegoalejndroflco@gmail.com>
2025-01-30 18:20:46 +0100

    Aggiustata grafica della schermata iniziale

--- Prolog/unocardgamegraphics.pl
+++ Prolog/unocardgamegraphics.pl
@@ -1,25 +1,28 @@
 :- use_module(library(pce)).
-load_image(Path):-
-    (   exists_file(Path) -> % Controlla se il file esiste
-        new(ImageObject, picture(Path)), % Carica l'immagine
-        send(@dialog, display, ImageObject)
-    ;   format('Errore: il file ~w non esiste.~n', [Path]) % Messaggio di errore
-    ).
 start_game :-
     free(@dialog), % @ prende indirizzo di memoria
     new(@dialog, dialog('Uno e mezzo')),
     send(@dialog, size, size(700,700)),
-    send(@dialog, background, green),
-    get(@dialog,size, size(W, H)),
-    load_image('C:/Users/diego/Documents/Prolog/Title_Image.jpeg'),
-    new(Label, text('UNO E MEZZO')),
-    send(Label, font, font(helvetica, bold, 20)),
+    send(@dialog, background, white),
+    get(@dialog,size, size(W, _)),
+    new(Imagefile, image('C:/Users/diego/Desktop/Prolog/UnoGameCard-on-prolog/Prolog/Title_Image.jpg')),
+    new(Bitmap, bitmap(Imagefile)),
+    send(@dialog, display, Bitmap),
+    send(Bitmap, center, @dialog?center),
+   % new(Label, text('UNO E MEZZO')),
+   %send(Label, font, font(helvetica, bold, 20)),
     new(Button, button('Inizia Gioco', message(@prolog, start_the_game))),
-    get(Label, size, size(Pw,Ph)),
-    X is (W-Pw)/2, % per centrare
-    Y is (H-Ph)/2,
-    send(@dialog, display, Label, point(X,10)),
-    send(@dialog, display, Button, point(X,60)),
+    send(Button, size, size(450, 100)), % Imposta la dimensione del bottone
+    send(Button, font, font(helvetica, bold, 30)), % Modifica il font del bottone
+    % Gestisci il cambiamento di colore quando il bottone è selezionato
+   % send(Button, message(Button, change_button_color, green)), % Colore quando selezionato
+   % send(Button, message(Button, change_button_color, grey), up), % Colore quando non selezionato
+    get(Bitmap,size,size(Pw,_)),
+    get(Button,size,size(Bw,_)),
+    Ximage is (W-Pw)/2, % per centrare
+    Xbutton is (W-Bw)/2,
+    send(@dialog, display, Bitmap, point(Ximage,10)),
+    send(@dialog, display, Button, point(Xbutton,500)),
     send(@dialog, open).
 
 start_the_game :-
